be56bf346a5ed95571f6cb3cf06f7a0d33f67ceb
anusi <anusi@Anusiya>
2023-04-21 14:22:34 +0530

    third commit

--- Helloworld1/src/main/java/com/demo/controller/HelloController.java
+++ Helloworld1/src/main/java/com/demo/controller/HelloController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/helloworld")
 	public String hello(){
-		return "Hi Hello World";
+		return "Hi Hello World Welcome to Web";
 			
+		
 	}
+	
 }
